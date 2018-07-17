57382926537622b26a9c018308578db8dd74e4e1
xxMiess <michelledekinkelder@gmail.com>
2018-07-17 14:03:26 +0200

    Buttons working
    - Play, Options & Quit buttons are working
    - Made a test build. Screen not matching up with actual gamescreen. Weird blue area on top of the screen.

---
+++ Bpw_The_way_of_gods/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //Loads the next level in the game if pressed on play
+    }
+
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit(); //Quits the game
+    }
+}
