bdc4e081dc3f6038b38e6ae8e82ec70ab84548a8
xxMiess <michelledekinkelder@gmail.com>
2018-07-31 15:46:02 +0200

    Added character movement and jumping
    Added character movent left & right and spacebar to jump. Can move right and jump, but not move left and jump. Don't know how to fix this, but it's not really a bug problem so far.

---
+++ Bpw_The_way_of_gods/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    public float jumpHeight;
+
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    private bool grounded;
+
+    private bool doubleJumped;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    private void FixedUpdate() {
+
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (grounded)
+            doubleJumped = false;
+
+		if(Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            //GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+            Jump();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && !doubleJumped && !grounded)
+        {
+            //GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+            Jump();
+            doubleJumped = true;
+        }
+
+            if (Input.GetKey(KeyCode.D))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.x);
+        }
+    }
+
+    public void Jump()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+    }
+}
